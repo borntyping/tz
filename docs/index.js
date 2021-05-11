3a794353cfffc4fcb8fe0f6519c78b7f93c87ff5
Sam Clements <sam@borntyping.co.uk>
2021-05-11 14:16:50 +0100

    Fix favicon

--- docs/index.js
+++ docs/index.js
@@ -137,7 +137,7 @@ class Favicon {
         const emoji = Favicon.getEmoji(localDateTime);
 
         redom.setAttr(this.el, {
-            href: `data:image/svg+xml,<svg xmlns="%22http://www.w3.org/2000/svg%22" viewBox="%220" 0 100 100%22><text y="%22.9em%22" font-size="%2290%22">${emoji}</text></svg>`
+            href: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`
         });
     }
 
